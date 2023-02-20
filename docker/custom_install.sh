6f91df6088a60848ee50822157925f45b9e44015
canoziia <canoziia@qq.com>
2023-02-21 00:17:37 +0800

    bugfix

--- docker/custom_install.sh
+++ docker/custom_install.sh
@@ -3,4 +3,5 @@ source "${venv_dir}"/bin/activate
 # install xformers and pytorch
 pip install --pre xformers
 pip install --pre torchvision
+pip install transformers==4.19.2 # 因为本地网络不好，版本升上去下载不了文件
 pip install triton
